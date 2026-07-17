fd2742b442904c6eb2c6f096ecd26292a7b072d7
Lalit Kishor Sahu <lksahu120206@gmail.com>
2026-07-17 10:55:21 +0530

    [207] [Time Beats: 75.20%] [Memory Beats: 44.50%] - Leet2Hub

--- Leet2Hub/207. Course Schedule/207. Course Schedule.cpp
+++ Leet2Hub/207. Course Schedule/207. Course Schedule.cpp
@@ -1,42 +1,47 @@
 1class Solution {
 2public:
-3    bool dfs(int node, vector<vector<int>>& adj,
-4             vector<int>& vis, vector<int>& pathVis) {
-5
-6        vis[node] = 1;
-7        pathVis[node] = 1;
-8
-9        for (int nbr : adj[node]) {
-10            if (!vis[nbr]) {
-11                if (dfs(nbr, adj, vis, pathVis))
-12                    return true;
-13            }
-14            else if (pathVis[nbr]) {
-15                return true;
+3    bool dfs(int node,
+4             vector<vector<int>>& adj,
+5             vector<int>& vis,
+6             vector<int>& pathVis) {
+7
+8        vis[node] = 1;
+9        pathVis[node] = 1;
+10
+11        for (int nbr : adj[node]) {
+12
+13            if (!vis[nbr]) {
+14                if (dfs(nbr, adj, vis, pathVis))
+15                    return true;
 16            }
-17        }
-18
-19        pathVis[node] = 0;
-20        return false;
-21    }
-22
-23    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-24
-25        vector<vector<int>> adj(numCourses);
-26
-27        for (auto &e : prerequisites)
-28            adj[e[1]].push_back(e[0]);
+17            else if (pathVis[nbr]) {
+18                return true;
+19            }
+20        }
+21
+22        pathVis[node] = 0;   // backtrack
+23        return false;
+24    }
+25
+26    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+27
+28        vector<vector<int>> adj(numCourses);
 29
-30        vector<int> vis(numCourses, 0);
-31        vector<int> pathVis(numCourses, 0);
-32
-33        for (int i = 0; i < numCourses; i++) {
-34            if (!vis[i]) {
-35                if (dfs(i, adj, vis, pathVis))
-36                    return false;
-37            }
-38        }
-39
-40        return true;
-41    }
-42};
+30        // b -> a
+31        for (auto &edge : prerequisites) {
+32            adj[edge[1]].push_back(edge[0]);
+33        }
+34
+35        vector<int> vis(numCourses, 0);
+36        vector<int> pathVis(numCourses, 0);
+37
+38        for (int i = 0; i < numCourses; i++) {
+39            if (!vis[i]) {
+40                if (dfs(i, adj, vis, pathVis))
+41                    return false;   // cycle found
+42            }
+43        }
+44
+45        return true;    // no cycle
+46    }
+47};
